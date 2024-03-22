9dc7352510bcd1251f1d36f856b02fb1b8aaae30
Diego Rebollo <diego_52h@proton.me>
2024-03-22 13:36:39 -0300

    [FEATURE](/): Prepare enviroment to run JavaFX applications
    	`pom.xml`: Add necessary plugins and dependencies for running JavaFX apps
    	`run.sh`: Convenience script for building and running the Kanban app
    
    [FEATURE](/code/ui): `App` class holds basic logic for running a JavaFX app
    
    	When run, the `App` class now creates and displays a simple (empty) JavaFX
    	scene

--- code/kanban/ui/App.java
+++ code/kanban/ui/App.java
@@ -1,10 +1,27 @@
 package kanban.ui;
 
-import kanban.logic.TaskManager;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
 
-class App
+public class App extends Application
 {
+	@Override
+	public void start(Stage stage)
+	{
+		HBox box = new HBox();
+		
+		Scene scene = new Scene(box, 640, 480);
+		
+		stage.centerOnScreen();
+		stage.setScene(scene);
+		
+		stage.show();
+	}
+	
 	public static void main(String args[])
 	{
+		launch(args);
 	}
 }

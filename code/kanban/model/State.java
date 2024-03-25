cc29cca69c87c2424e7f96498c570e5cc7b87137
Diego Rebollo <diego_52h@proton.me>
2024-03-25 17:06:36 -0300

    [FEATURE](/code/kanban/model): Change the model API
    [FEATURE](/docs): Modify UML diagrams to reflect the model API changes

---
+++ code/kanban/model/State.java
@@ -0,0 +1,8 @@
+package kanban.model;
+
+public enum State
+{
+	TO_DO,
+	ON_IT,
+	DONE,
+}
--- code/kanban/model/Task.java
+++ code/kanban/model/Task.java
@@ -1,16 +1,34 @@
 package kanban.model;
 
+import kanban.model.State;
+
 public class Task
 {
 	private String name;
+	private String data;
 	
-	public Task(String name)
+	private String color;
+	
+	private State state;
+	
+	public Task(String name, String data, String color)
 	{
 		this.name = name;
+		this.data = data;
+		this.color = color;
+		
+		this.state = State.TO_DO;
 	}
 	
-	public String getName()
-	{
-		return this.name;
-	}
+	public String getName() { return this.name; }
+	public String getData() { return this.name; }
+	public String getColor() { return this.name; }
+	
+	public State getState() { return this.state; }
+	
+	public void setName(String name) { this.name = name; }
+	public void setData(String data) { this.data = data; }
+	public void setColor(String color) { this.color = color; }
+	
+	public void setState(State state) { this.state = state; }
 }

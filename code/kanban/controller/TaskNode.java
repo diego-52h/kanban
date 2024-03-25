d8cd62fc7c1fd0afcc35d3151f32db916f1eca7f
Diego Rebollo <diego_52h@proton.me>
2024-03-25 19:21:39 -0300

    [FEATURE](/code/kanban/controller): Integrate model into `TaskNode` class
    [FIX](/docs): Fix UML diagram inheritance arrow directions
    
    [FEATURE](/docs): Update diagram with new functionality

--- code/kanban/controller/TaskNode.java
+++ code/kanban/controller/TaskNode.java
@@ -1,5 +1,7 @@
 package kanban.controller;
 
+import javafx.beans.property.SimpleObjectProperty;
+
 import javafx.event.ActionEvent;
 
 import javafx.fxml.FXML;
@@ -13,12 +15,18 @@ import javafx.scene.input.Dragboard;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.input.TransferMode;
 
+import javafx.scene.paint.Color;
+
 import kanban.controller.EditorWindow;
+
+import kanban.model.Task;
 
 public class TaskNode extends Label
 {
 	private @FXML MenuItem modifyButton;
 	private @FXML MenuItem removeButton;
+	
+	private SimpleObjectProperty<Task> task;
 	
 	public TaskNode()
 	{
@@ -31,7 +39,8 @@ public class TaskNode extends Label
 		{
 			loader.load();
 			
-			this.setText("task");
+			this.task = new SimpleObjectProperty(new Task("", "", "#FEBF91"));
+			this.task.addListener((elem, prev, curr) -> { this.update(); });
 			
 			this.modifyButton.setOnAction((ActionEvent event) -> { this.modify(); event.consume(); });
 			this.removeButton.setOnAction((ActionEvent event) -> { this.remove(); event.consume(); });
@@ -45,12 +54,28 @@ public class TaskNode extends Label
 				
 				event.consume();
 			});
+			
+			this.setOnMouseExited((MouseEvent event) -> { this.setColor(Color.web(this.getTask().getColor())); event.consume(); });
+			this.setOnMouseEntered((MouseEvent event) -> { this.setColor(Color.web(this.getTask().getColor()).deriveColor(1, 0.8, 1, 1)); event.consume(); });
+			
+			this.update();
 		}
 		
 		catch(Exception exception)
 		{
 			exception.printStackTrace();
 		}
+	}
+	
+	public Task getTask()
+	{
+		return this.task.getValue();
+	}
+	
+	private void update()
+	{
+		this.setText(this.getTask().getName());
+		this.setColor(Color.web(this.getTask().getColor()));
 	}
 	
 	private void modify()
@@ -61,5 +86,16 @@ public class TaskNode extends Label
 	private void remove()
 	{
 		System.out.println("remove");
+	}
+	
+	private void setColor(Color color)
+	{
+		int r = (int) (255 * color.getRed());
+		int g = (int) (255 * color.getGreen());
+		int b = (int) (255 * color.getBlue());
+		
+		String cssSetter = String.format("-fx-background-color: rgb(%d, %d, %d);", r, g, b);
+		
+		this.setStyle(cssSetter);
 	}
 }

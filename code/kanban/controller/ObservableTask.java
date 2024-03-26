2b675e87c98c596f48c10c5a87276df8361a9588
Diego Rebollo <diego_52h@proton.me>
2024-03-26 17:01:46 -0300

    [FEATURE](/code/kanban/controller): Create an `ObservableTask` class
    	Class is a wrapper for the `Task` class, with the distinction that it stores
    	a list of `listeners` to notify when some data is modified.
    
    	Part of the observer pattern.

---
+++ code/kanban/controller/ObservableTask.java
@@ -0,0 +1,61 @@
+package kanban.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javafx.beans.Observable;
+import javafx.beans.InvalidationListener;
+
+import kanban.model.State;
+import kanban.model.Task;
+
+public class ObservableTask extends Task implements Observable
+{
+	private List<InvalidationListener> listeners = new ArrayList();
+	
+	public ObservableTask(String name, String data, String color, State state)
+	{
+		super(name, data, color, state);
+	}
+	
+	@Override
+	public void setName(String name)
+	{
+		super.setName(name);
+		
+		this.notifyListeners();
+	}
+	
+	@Override
+	public void setData(String data)
+	{
+		super.setData(data);
+		
+		this.notifyListeners();
+	}
+	
+	@Override
+	public void setColor(String color)
+	{
+		super.setColor(color);
+		
+		this.notifyListeners();
+	}
+	
+	@Override
+	public void setState(State state)
+	{
+		super.setState(state);
+		
+		this.notifyListeners();
+	}
+	
+	@Override public void addListener(InvalidationListener listener) { this.listeners.add(listener); }
+	@Override public void removeListener(InvalidationListener listener) { this.listeners.remove(listener); }
+	
+	private void notifyListeners()
+	{
+		for(InvalidationListener listener : this.listeners)
+			listener.invalidated(this);
+	}
+}
